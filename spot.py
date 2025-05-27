c4e8ac55d4ffdfe42555422ac03c7310594435fc
Mohammed Hassan <111278451+MohdHassanS@users.noreply.github.com>
2025-05-27 13:35:28 +0530

    spot.py

--- spot.py
+++ spot.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
