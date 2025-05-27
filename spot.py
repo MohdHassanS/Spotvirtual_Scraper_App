ee91d2dcabddda72c87e089ed6c18019a61e474f
Mohammed Hassan <111278451+MohdHassanS@users.noreply.github.com>
2025-05-27 13:01:16 +0530

    spot.py

--- spot.py
+++ spot.py
@@ -17,17 +17,13 @@ st.title("👥 Hey Guvi'ans! Let see who was there with us in the SpotVirtual...
 
 if 'driver' not in st.session_state:
     with st.spinner("Wait for it...", show_time=True):
-        # Get chromium path
-        CHROME_BINARY_PATH = "/usr/bin/chromium"  # Render installs it here
-        
-        options = webdriver.ChromeOptions()
-        options.binary_location = CHROME_BINARY_PATH
+        options = uc.ChromeOptions()
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=options)
+        driver = uc.Chrome(options=options)
+        driver.maximize_window()
         st.session_state.driver = driver
 
 
