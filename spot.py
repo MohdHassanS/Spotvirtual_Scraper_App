ab4f1f8458eecd61be38d9cfc408bee62d599408
Mohammed Hassan <111278451+MohdHassanS@users.noreply.github.com>
2025-05-27 18:05:57 +0530

    spot.py

--- spot.py
+++ spot.py
@@ -73,9 +73,17 @@ def scrape_names(driver=st.session_state.driver):
         screenshot = driver.get_screenshot_as_png()
         st.image(BytesIO(screenshot), caption='before scraping')
         try:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//div[4]//div[2]//a[1]"))).click()
+        except:
+            st.write("No show all 1")
+        try:
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//div[5]//div[2]//a[1]"))).click()
         except:
-            st.write("No show all in members")
+            st.write("No show all 2")
+        try:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//div[6]//div[2]//a[1]"))).click()
+        except:
+            st.write("No show all 3")
         # for xpath in ["//div[4]//div[2]//a[1]","//div[5]//div[2]//a[1]","//div[6]//div[2]//a[1]"]:
         #     try:
         #         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,xpath))).click()
