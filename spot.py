c9d9e9924a6f6e96be2d7decd80909b52c5965a3
Mohammed Hassan <111278451+MohdHassanS@users.noreply.github.com>
2025-05-27 13:05:38 +0530

    spot.py

--- spot.py
+++ spot.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import undetected_chromedriver as uc
 from webdriver_manager.chrome import ChromeDriverManager
 from io import BytesIO
 import traceback
