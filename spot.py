66ad87c2a5646f7784e7a035b785656da36c130f
Mohammed Hassan <111278451+MohdHassanS@users.noreply.github.com>
2025-05-28 14:51:07 +0530

    spot.py

--- spot.py
+++ spot.py
@@ -75,21 +75,21 @@ def confirm_verification_code(code, driver=st.session_state.driver):
 def scrape_names(driver=st.session_state.driver):
     with st.spinner("Wait for it...", show_time=True):
         time.sleep(5)
-        screenshot = driver.get_screenshot_as_png()
-        st.image(BytesIO(screenshot), caption='before scraping')
+        # screenshot = driver.get_screenshot_as_png()
+        # st.image(BytesIO(screenshot), caption='before scraping')
         
-        # if driver.find_elements(By.XPATH, "//div[4]//div[2]//a[1]"):
-        #     element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//div[4]//div[2]//a[1]")))
-        #     driver.execute_script("arguments[0].click();", element)
-        #     print("Found Show all 4")
+        if driver.find_elements(By.XPATH, "//div[4]//div[2]//a[1]"):
+            element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//div[4]//div[2]//a[1]")))
+            driver.execute_script("arguments[0].click();", element)
+            st.write("Found Show all 4")
         if driver.find_elements(By.XPATH, "//div[5]//div[2]//a[1]"):
             element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//div[5]//div[2]//a[1]")))
             driver.execute_script("arguments[0].click();", element)
-            print("Found Show all 5")
+            st.write("Found Show all 5")
         if driver.find_elements(By.XPATH, "//div[6]//div[2]//a[1]"):
             element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//div[6]//div[2]//a[1]")))
             driver.execute_script("arguments[0].click();", element)
-            print("Found Show all 6")
+            st.write("Found Show all 6")
 
         element = driver.find_elements(By.XPATH, "//div[contains(@class, 'OrgSidebar_scrollContainer')]")
         text_content = "\n".join([el.text for el in element])
