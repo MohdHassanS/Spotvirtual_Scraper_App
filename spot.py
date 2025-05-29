b571d7361f36a1a54dbea4f4689081bc26d354f2
Mohammed Hassan <111278451+MohdHassanS@users.noreply.github.com>
2025-05-29 17:28:34 +0530

    spot.py

--- spot.py
+++ spot.py
@@ -78,16 +78,17 @@ def scrape_names(driver=st.session_state.driver):
 
         try:
             show_all_button = driver.find_element(By.LINK_TEXT, "Show all")
-            time.sleep(3)
             show_all_button.click()
-            st.write("Found Show all")
+            st.write("Found Show all 1")
             time.sleep(3)
+            show_all_button = driver.find_element(By.LINK_TEXT, "Show all")
+            show_all_button.click()
+            st.write("Found Show all 2")
         except: 
             st.write("Not Found Show all")
 
         try:
-            # sidebar = driver.find_element(By.ID, "root")
-            sidebar = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "root")))
+            sidebar = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "Sidebar_sidebar__olcdO")))
             sidebar_text = sidebar.text
             spt = sidebar_text.splitlines()
             unnes = ['New ZEN DS','Browse spaces','Browse channels',"You haven't joined any channels",'GUESTS','Office',' Invite teammates','MEMBERS','SPACES','CHANNELS','Show less','ADMIN']
@@ -98,25 +99,6 @@ def scrape_names(driver=st.session_state.driver):
             error_details = traceback.format_exc()
             st.code(error_details, language='python')
             return []
-        # xpaths = ["//div[4]//div[2]//a[1]", "//div[5]//div[2]//a[1]", "//div[6]//div[2]//a[1]"]
-        # for i, xpath in enumerate(xpaths, start=4):
-        #     try:
-        #         elem = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-        #         driver.execute_script("arguments[0].click();", elem)
-        #         st.write(f"Clicked: Show all {i}")
-        #         time.sleep(3)
-        #     except: pass
-
-        # try:
-        #     containers = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'OrgSidebar_scrollContainer')]")))
-        # except:
-        #     st.error("Failed to locate member list section.")
-        #     return []
-
-        # raw = "\n".join([el.text[:1000] for el in containers if el.text.strip()])
-        # unwanted = {'Browse channels','Browse spaces',"You haven't joined any channels",'GUESTS','Office',' Invite teammates','MEMBERS','SPACES','CHANNELS','Show less','ADMIN'}
-        # return sorted([n for n in set(raw.splitlines()) if len(n) > 2 and n not in unwanted])
-
 
 if 'email' not in st.session_state:
     st.session_state.email = ''
