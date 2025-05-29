10b7f9d97259697691c211604d7a265faa3f4f15
Mohammed Hassan <111278451+MohdHassanS@users.noreply.github.com>
2025-05-29 12:21:26 +0530

    spot.py

--- spot.py
+++ spot.py
@@ -76,14 +76,21 @@ def scrape_names(driver=st.session_state.driver):
     with st.spinner("Scraping...",show_time=True):
         time.sleep(3)
 
-        xpaths = ["//div[4]//div[2]//a[1]", "//div[5]//div[2]//a[1]", "//div[6]//div[2]//a[1]"]
-        for i, xpath in enumerate(xpaths, start=4):
-            try:
-                elem = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-                driver.execute_script("arguments[0].click();", elem)
-                st.write(f"Clicked: Show all {i}")
-                time.sleep(3)
-            except: pass
+        try:
+            show_all_button = driver.find_element(By.LINK_TEXT, "Show all")
+            time.sleep(3)
+            show_all_button.click()
+            st.write("Found Show all")
+        except: 
+            st.write("Not Found Show all")
+        # xpaths = ["//div[4]//div[2]//a[1]", "//div[5]//div[2]//a[1]", "//div[6]//div[2]//a[1]"]
+        # for i, xpath in enumerate(xpaths, start=4):
+        #     try:
+        #         elem = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        #         driver.execute_script("arguments[0].click();", elem)
+        #         st.write(f"Clicked: Show all {i}")
+        #         time.sleep(3)
+        #     except: pass
 
         try:
             containers = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'OrgSidebar_scrollContainer')]")))
